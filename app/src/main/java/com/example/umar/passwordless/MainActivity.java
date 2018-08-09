c083c4cfc017c0d8ced30c7950f49f2257e44b3d
umarfarooq63114 <39738247+umarfarooq63114@users.noreply.github.com>
2018-08-09 13:50:46 +0500

    Initial commit

---
+++ app/src/main/java/com/example/umar/passwordless/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.umar.passwordless;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnPhone, btnEmail;
+    private final static int REQUEST_CODE = 999;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnEmail = findViewById(R.id.btnEmail);
+        btnPhone = findViewById(R.id.btnPhone);
+
+
+        // button workin with phone
+        btnPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startLoginPage(LoginType.PHONE);
+
+            }
+        });
+
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startLoginPage(LoginType.EMAIL);
+
+            }
+        });
+
+
+
+
+
+    }
+
+    private void startLoginPage(LoginType loginType) {
+        if (loginType == LoginType.EMAIL) {
+            Intent intent = new Intent(this, AccountKitActivity.class);
+            AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                    new AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.EMAIL,
+                            AccountKitActivity.ResponseType.CODE);
+            intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, configurationBuilder.build());
+            startActivityForResult(intent, REQUEST_CODE);
+        }
+
+        else if (loginType == LoginType.PHONE) {
+            Intent intent = new Intent(this, AccountKitActivity.class);
+            AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                    new AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.PHONE,
+                            AccountKitActivity.ResponseType.CODE);
+            intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, configurationBuilder.build());
+            startActivityForResult(intent, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+
+            AccountKitLoginResult result = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            if (result.getError() != null) {
+                Toast.makeText(this, "" + result.getError()
+                        .getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
+                return;
+            } else if (result.wasCancelled()) {
+                Toast.makeText(this, "cancel", Toast.LENGTH_SHORT).show();
+
+                return;
+
+
+               /* Toast.makeText(this, "Success ! %s "+result.getAuthorizationCode()
+                        .substring(0,10), Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this,Success.class));*/
+            } else {
+                if (result.getAccessToken() != null) {
+                    Toast.makeText(this, "Success ! " + result.getAccessToken().getAccountId()
+                            , Toast.LENGTH_SHORT).show();
+                } else {
+
+                    Toast.makeText(this, "Success ! "+result.getAuthorizationCode()
+                            .substring(0,10), Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(this, Success.class));
+
+                }
+            }
+
+
+        }
+
+    }
+
+
+    private void printKeyHash() {
+    try{
+        PackageInfo info=getPackageManager().getPackageInfo("com.example.umar.passwordless",
+                PackageManager.GET_SIGNATURES);
+        for(Signature signature: info.signatures)
+        {
+            MessageDigest md= MessageDigest.getInstance("SHA");
+            md.update(signature.toByteArray());
+            Log.d("MYKEY", Base64.encodeToString(md.digest(),Base64.DEFAULT));
+        }
+
+
+    } catch (PackageManager.NameNotFoundException e) {
+        e.printStackTrace();
+    } catch (NoSuchAlgorithmException e) {
+        e.printStackTrace();
+    }
+    }
+}
---
+++ app/src/main/java/com/example/umar/passwordless/Success.java
@@ -0,0 +1,85 @@
+package com.example.umar.passwordless;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+
+public class Success extends AppCompatActivity {
+
+    Button btnLogout;
+    TextView tvPhone;
+
+    EditText etUserId,etUserPhone,etUserEmail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+        btnLogout=findViewById(R.id.btnLogout);
+
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AccountKit.logOut();
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+            @Override
+            public void onSuccess(Account account) {
+                Toast.makeText(Success.this, "yes", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(AccountKitError accountKitError) {
+                Toast.makeText(Success.this, "no", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /*@Override
+    protected void onResume() {
+        super.onResume();
+        AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+            @Override
+            public void onSuccess(Account account) {
+
+                tvPhone=findViewById(R.id.tvPhone);
+                etUserEmail=findViewById(R.id.etUserEmail);
+                etUserEmail.setText(String.format("Email Id %s",account.getEmail()));
+
+
+                tvPhone.setText(String.format("Phone %s",account.getPhoneNumber() == null ? "": account.getPhoneNumber().toString()));
+
+
+                etUserId=findViewById(R.id.etUserId);
+                etUserId.setText(String.format("User Id %s",account.getId()));
+
+                etUserPhone=findViewById(R.id.etUserPhone);
+                //etUserPhone.setText(String.format("Phone %s",account.getPhoneNumber() == null ?
+                        //"": account.getPhoneNumber().toString()));
+
+
+
+            }
+
+            @Override
+            public void onError(AccountKitError accountKitError) {
+                Toast.makeText(Success.this, "Umar farooq", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }*/
+}
